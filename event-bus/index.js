1f96da3a776a46810e0043a417bd7c1d14b359b6
Orçun <manaporcun@gmail.com>
2022-03-19 17:01:58 +0300

    Local to github

---
+++ event-bus/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const app = express();
+app.use(bodyParser.json());
+
+const events = [];
+
+app.post("/events", (req, res) => {
+  const event = req.body;
+
+  events.push(event);
+
+  axios.post("http://posts-clusterip-srv:4000/events", event).catch((err) => {
+    console.log(err.message, ":4000");
+  });
+
+  axios.post("http://comments-srv:4001/events", event).catch((err) => {
+    console.log(err.message, ":4001");
+  });
+
+  axios.post("http://query-srv:4002/events", event).catch((err) => {
+    console.log(err.message, ":4002");
+  });
+
+  axios.post("http://moderation-srv:4003/events", event).catch((err) => {
+    console.log(err.message, ":4003");
+  });
+
+  res.send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
+});
+
+app.listen(4005, () => {
+  console.log("listening on 4005");
+});
